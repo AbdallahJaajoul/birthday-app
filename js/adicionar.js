e18004d375d228690f03aa24fa6e8d2a7d49cb68
abdallah <abdullahmahercacul@gmail.com>
2025-09-19 22:16:30 +0100

    Update adicionar.js

--- js/adicionar.js
+++ js/adicionar.js
@@ -1,8 +1,6 @@
-const form = document.getElementById('formAdicionar');
-const listaMembros = document.getElementById('listaMembros');
-let membros = JSON.parse(localStorage.getItem('membros')) || [];
-
-function renderizar() {
-  listaMembros.innerHTML = '';
-  membros.forEach((m,i) => {
-    const li =
+fetch('membros.json')
+  .then(res => res.json())
+  .then(data => {
+    membros = data;
+    renderizar();
+  });
